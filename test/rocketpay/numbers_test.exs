758dae577858afecc5076faf38203068438a109a
Gabriel Kim <gabrielkim13@gmail.com>
2021-02-25 17:18:38 -0300

    1st lesson - Setup Elixir + Phoenix project. - Add first route to sum a comma-separated list of numbers.

---
+++ test/rocketpay/numbers_test.exs
@@ -0,0 +1,23 @@
+defmodule Rocketpay.NumbersTest do
+  use ExUnit.Case
+
+  alias Rocketpay.Numbers
+
+  describe "sum_from_file/1" do
+    test "should return the sum of the comma-separated numbers, when the file is valid" do
+      response = Numbers.sum_from_file("numbers")
+
+      expected_response = {:ok, %{result: 33}}
+
+      assert response == expected_response
+    end
+
+    test "should return an error, when the file is invalid" do
+      response = Numbers.sum_from_file("invalid")
+
+      expected_response = {:error, %{message: "Invalid file!"}}
+
+      assert response == expected_response
+    end
+  end
+end
